4fe77dd01c2c541493e67adbddb726db444a9fff
Azuby <83681090+Azuby-hash@users.noreply.github.com>
2026-05-08 12:01:14 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,22 +1,38 @@
-// swift-tools-version:5.7
-
+// swift-tools-version:5.9
 import PackageDescription
 
-
-let dependencies = [
-  Package.Dependency.package(url: "https://github.com/kylef-archive/CommonCrypto.git", from: "1.0.0"),
-]
-
 let package = Package(
-  name: "JWT",
-  products: [
-    .library(name: "JWT", targets: ["JWT"]),
-  ],
-  dependencies: dependencies,
-  targets: [
-    .target(name: "JWA", dependencies: targetDependencies, exclude: excludes),
-    .target(name: "JWT", dependencies: ["JWA"]),
-    .testTarget(name: "JWATests", dependencies: ["JWA"]),
-    .testTarget(name: "JWTTests", dependencies: ["JWT"]),
-  ]
+    name: "JWT",
+    platforms: [
+        .macOS(.v13),
+        .iOS(.v16),
+        .watchOS(.v9),
+        .tvOS(.v16),
+    ],
+    products: [
+        .library(name: "JWT", targets: ["JWT"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.8.0"),
+    ],
+    targets: [
+        .target(
+            name: "JWA",
+            dependencies: ["CryptoSwift"],
+            exclude: ["HMAC/HMACCommonCrypto.swift"]
+        ),
+        .target(
+            name: "JWT",
+            dependencies: ["JWA"]
+        ),
+        .testTarget(
+            name: "JWATests",
+            dependencies: ["JWA"]
+        ),
+        .testTarget(
+            name: "JWTTests",
+            dependencies: ["JWT"]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
 )
