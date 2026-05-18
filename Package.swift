706c6ba0999eed9690cab270b88c65a8dea3aff9
Azuby <83681090+Azuby-hash@users.noreply.github.com>
2026-05-18 08:47:09 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "JWT", targets: ["JWT"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.0.0"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", exact: "1.0.0"),
     ],
     targets: [
         .target(
